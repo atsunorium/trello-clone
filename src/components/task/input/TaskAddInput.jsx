7f56efd5c7586105d9a8dd9b5018ac1e1c671f8b
atsunorium <65150911+atsunorium@users.noreply.github.com>
2022-02-02 11:29:26 +0900

    セクション6完了

--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,5 +1,43 @@
 import React from 'react';
+import { v4 as uuid } from 'uuid';
 
-export const TaskAddInput = () => {
-  return <div></div>;
+export const TaskAddInput = ({
+  inputText, 
+  setInputText, 
+  setTaskList, 
+  taskList,
+}) => {
+  const handleSubmit = (e) => {
+    // uuidの生成
+    const taskId = uuid();
+    e.preventDefault();//リロードの無効化。
+    if(inputText === ""){
+      return;
+    }
+    // カードを追加する。
+    setTaskList([...taskList, 
+      {
+        id: taskId,
+        draggable: `task-${taskId}`,
+        text: inputText,
+      },
+  ]);
+    setInputText("");
+  };
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
+  return (
+  <div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" 
+      placeholder="add a task" 
+      className="taskAddInput" 
+      onChange={handleChange}
+      value={inputText}
+      />
+    </form>
+  </div>
+  );
 };
